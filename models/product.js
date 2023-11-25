2f6e63c73538b1393bf44dd62aa3a7da8def807c
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-11-25 11:14:43 +0530

    Adding a detail button in form of anchor tag to the products and whgen we click on it we get the product id which is passed in the routes and then to the controller to send the user data from products to the home page.

---
+++ models/product.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    // providing a unique id that we will use in the href in the details button for product clicked.
+
+    this.id = Math.random().toString(); //now all the products wiull have an id too
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
